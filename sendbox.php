8e2ace628d6f034505ea405215fade3cc52a84b1
Lida <eganyan.lida22@gmail.com>
2025-10-15 15:26:19 +0400

    PHP Tutorial (& MySQL) #34 - Ternary Operators

---
+++ sendbox.php
@@ -0,0 +1,23 @@
+<?php 
+$score = 50;
+// if($score > 40){
+//    echo 'high score!';
+
+// }else {
+//    echo 'low score :(';
+// }
+
+// $val = $score > 40 ? 'high score :)' : 'low score :(';
+// echo $val;
+?>
+
+
+<!DOCTYPE html> 
+<html>
+<head>
+   <title> </title>
+</head>
+<body>
+   <p><?php echo $score > 40 ? 'high score :)' : 'low score :(';?></p>
+</body>
+</html>
